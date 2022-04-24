5e9d894d8a05f48310e40359073b518c563cdd05
Alan Boulay <boulay.alan@gmail.com>
2022-04-24 17:14:36 +0200

    Update computed js file

--- Vue-basics-concepts/3-Computed/app.js
+++ Vue-basics-concepts/3-Computed/app.js
@@ -1,10 +1,12 @@
-
 const app = {
 
     data () {
         return {
             nb1: 42,
-            nb2: 35
+            nb2: 35,
+
+            nom: "Deblouze",
+			prenom: "Agathe"
     }
 },
     computed: {
@@ -14,8 +16,21 @@ const app = {
                 let dnb2 = Math.trunc(this.nb2/10);
                 let unb2 = this.nb2-dnb2*10;
                 return 10*dnb1+unb2;
-        }
+        },
+
+        // Get + Set
+        nomComplet : {
+			get () {
+				return this.prenom+' '+this.nom;
+			},
+			set (prenom_nom) {
+				let nom = prenom_nom.split(" ");
+				this.prenom = nom[0];
+				this.nom = nom[1];
+			}
+		}
     }
 };
 
 Vue.createApp(app).mount("#app");
+
